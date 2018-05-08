e4a381b5059d64d4e46a78af86c67e83c3f11e11
thomere <thomere@thomere-PC.home>
2018-05-08 22:29:37 +0200

    images : change from absolute to relative path

--- MinesWeeperPanelBasedPattern/src/images/ImageHandler.java
+++ MinesWeeperPanelBasedPattern/src/images/ImageHandler.java
@@ -11,7 +11,7 @@ public class ImageHandler {
 	private static ImageIcon bombIcon;
 	private static ImageIcon flagIcon;
 	private static ImageIcon hiddenIcon;
-	private static String path = "C:/Users/thomere/Dropbox/workspace/Games/MinesWeeperFrameBasedPattern/images/";
+	private static String path = "bin/";
 
 	public static void createImages() {
 		setBombIcon(new ImageIcon(path + "demineur.png"));
